788d51ba79ed747d77be15a95953dbf0c44e1ebd
Josh Hernandez <Josh.Hernandez@norimaconsulting.com>
2018-01-04 17:07:41 -0600

    added comments to the test file for the different sections in the file

--- test.sh
+++ test.sh
@@ -10,6 +10,7 @@ test_params="--show-args"
 echo "Starting Test" && \
     $python_exe $dice_distro_file $test_params --help && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 && \
+    # Test basic operations
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply sum && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply min && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply max && \
@@ -18,19 +19,25 @@ echo "Starting Test" && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply bit-or && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply bit-xor && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply bit-and && \
+    # Test basic operations with parameters
     $python_exe $dice_distro_file $test_params -d 6 -n 4 --apply sum  2 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply shift 2 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply shift 2 -1 && \
+    # Test scale operations with all the different rounding options
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply scale r-ceil 0.5 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply scale r-floor 0.5 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply scale r-truncate 0.5 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply scale r-half-up 0.5 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply scale r-half-down 0.5 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply scale 0.5 0.333 prod && \
+    # Test bound
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply bound 3 4 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply bound 2 5 3 4 && \
+    # Test chained operation with bound
     $python_exe $dice_distro_file $test_params -d 6 -n 1 --apply shift 2 bound 1 6 && \
+    # Test if-block
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply shift if mod 2 3 eq 0 1 then 10 100 && \
+    # Test Select operation
     $python_exe $dice_distro_file $test_params -d 6 -n 4 --apply select 1 && \
     $python_exe $dice_distro_file $test_params -d 4 -n 4 --apply select -1 -2 -3 && \
     $python_exe $dice_distro_file $test_params -d 4 -n 4 --apply select -1 -2 -3 --sort value && \
@@ -40,23 +47,31 @@ echo "Starting Test" && \
     $python_exe $dice_distro_file $test_params -d 8 -n 5 --apply select -1 -2 -3 select 0 1 select 1 && \
     $python_exe $dice_distro_file $test_params -d 8 -n 5 --apply select -1 -4 -2 -3 sum 2 select 0 && \
     $python_exe $dice_distro_file $test_params -d 8 -n 5 --apply select -1 -2 -3 select 0 1 select 1 --memorize && \
+    # Test Reroll
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply reroll if lt 4 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 4 --apply sum 2 reroll if lt 7 && \
-    $python_exe $dice_distro_file $test_params -d 6 -n 4 --apply slice-apply 2 sum max && \
     $python_exe $dice_distro_file $test_params -d 6 -n 4 --apply reroll if mod 2 eq 1 && \
+    # Test slice-apply
+    $python_exe $dice_distro_file $test_params -d 6 -n 4 --apply slice-apply 2 sum max && \
+    # Test custom single die type options
     $python_exe $dice_distro_file $test_params -d 10 -n 2 --die-start 0 && \
     $python_exe $dice_distro_file $test_params -d 10 -n 2 --die-start 0 --die-step 10 && \
     $python_exe $dice_distro_file $test_params -n 2 --die-values 0 10 100 -1000 && \
+    # Test custom multi-die type options
     $python_exe $dice_distro_file $test_params --multi-die-sides 12 8 6 --apply sum && \
     $python_exe $dice_distro_file $test_params --multi-die-sides 8 6 4 --apply reroll if lt 3 && \
     $python_exe $dice_distro_file $test_params --multi-die-sides 8 6 4 --apply reroll if lt 5 4 && \
     $python_exe $dice_distro_file $test_params --multi-die-sides 4 3 2 --multi-die-start -1 0 1 --multi-die-step 3 2 1 && \
     $python_exe $dice_distro_file $test_params --multi-die-sides 4 3 2 --multi-die-values 0 1 2 3 10 20 30 100 200 && \
-    # the following two tests are related
+    # Test file save and load with distrobution product
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --save /tmp/2d6.json && \
     $python_exe $dice_distro_file $test_params --load /tmp/2d6.json /tmp/2d6.json --apply sum && \
+    # Test display options
+    $python_exe $dice_distro_file $test_params -d 6 -n 2 --no-output && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply sum --show-counts && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply sum --sort value && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply sum --bar-size 0 && \
     $python_exe $dice_distro_file $test_params -d 6 -n 2 --apply sum --bar-size 2 --bar-char '@#' --bar-prefix '<|' && \
-    $python_exe $dice_distro_file $test_params -d 40 -n 6 --apply max --simulate 100000
+    # Test simulate
+    $python_exe $dice_distro_file $test_params -d 40 -n 6 --apply max --simulate 100000 && \
+    echo "End of Test"
