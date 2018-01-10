db1c95dfd9be3f55421a86c1c4f3ff5d3be7d808
Josh Hernandez <i.am.mr.josh.hernandez@gmail.com>
2018-01-09 23:17:37 -0600

    added tests for the parser features

--- test/test_operation_parser.py
+++ test/test_operation_parser.py
@@ -17,8 +17,7 @@ class TestParserBase(unittest.TestCase):
     def parse_command(self,command):
         command_list = command.split()
         return dice_distro.get_operator(
-            operation_str=command_list[0],
-            param_list=command_list[1:],
+            param_list=command_list,
             should_validate=True,
         )
 
@@ -31,7 +30,7 @@ class TestParserBase(unittest.TestCase):
             self.assertEqual(test_func(value_set), answer_func(value_set))
 
 class TestOperationParser(TestParserBase):
-    def test1(self):
+    def test_independent_index_01(self):
         operation_func = self.parse_command("""
             add 100 if mod 5 eq 2 else add 10 if mod 2 eq 1 else scale 0
         """)
@@ -48,7 +47,24 @@ class TestOperationParser(TestParserBase):
             index_independent_test_value_set,
         )
 
-    def test2(self):
+    def test_independent_index_02(self):
+        operation_func = self.parse_command("""
+            [ add 2 scale 2 ] if mod 3 eq 1 else [ scale -1 add -1 ] if mod 3 eq 2 else exp 3
+        """)
+
+        def expected_result(xx):
+            return tuple(
+                ( (ii+2)*2 ) if ii % 3 == 1 else (-ii-1) if ii % 3 == 2 else ii**3
+                for ii in xx
+            )
+
+        self.run_operation_against_collection(
+            operation_func,
+            expected_result,
+            index_independent_test_value_set,
+        )
+
+    def test_dependent_index_01(self):
         add1 = [ 9, 11, 13, 17, 23, 29]
         mod1 = [ 2, 3, 4, 5,  6,  7]
         cmp1 = [ 1, 2, 3, 4,  3,  2]
